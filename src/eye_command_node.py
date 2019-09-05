2a79fc2618706e85504782170968f6b6ba2f9589
leon-liangwu <wuliang0301@hotmail.com>
2019-09-05 22:32:13 +0800

    face detection and landmarks

--- src/eye_command_node.py
+++ src/eye_command_node.py
@@ -13,29 +13,27 @@ from tf_utils import conv2d, dilated2d, max_pool_2x2, weight_variable, bias_vari
 
 import cv2
 import numpy as np
+import dlib
 
+# parameters setting
+cap_region_x_begin=0.5  # start point/total width
+cap_region_y_end=0.8  # start point/total width
+
+# parameter 1
+threshold = 40  #  BINARY threshold
+
+blurValue = 41  # GaussianBlur parameter
+bgSubThreshold = 50
+learningRate = 0
+
+# variables
+isBgCaptured = 0   # bool, whether the background captured
+triggerSwitch = False  # if true, keyborad simulator works
+skinkernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
 
 if __name__ == '__main__':
-    global cap_region_x_begin,cap_region_y_end,threshold,blurValue,bgSubThreshold,learningRate,isBgCaptured,triggerSwitch,skinkernel
     
-    print('hola')
-    # parameters
-    cap_region_x_begin=0.5  # start point/total width
-    cap_region_y_end=0.8  # start point/total width
-
-    # parameter 1
-    threshold = 40  #  BINARY threshold
-
-
-    blurValue = 41  # GaussianBlur parameter
-    bgSubThreshold = 50
-    learningRate = 0
-
-    # variables
-    isBgCaptured = 0   # bool, whether the background captured
-    triggerSwitch = False  # if true, keyborad simulator works
-    skinkernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-    
+    print('Starting...')
     
     #added from ZC's code 
     parser = argparse.ArgumentParser()
@@ -51,14 +49,41 @@ if __name__ == '__main__':
     parser.add_argument("--camera_mat", type=str,
                         default='./asserts/camera_matrix.mat',
                             help="Path to camera matrix")
-    FLAGS, unparsed = parser.parse_known_args()
-    # tf.app.run()
-    # tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
-    
-    #added from ZC's code
 
-#     try:
-#         talker()
-#     except rospy.ROSInterruptException:
-#         pass
-# ``
+    FLAGS, unparsed = parser.parse_known_args()
+
+    print(FLAGS.camera_mat)
+
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(FLAGS.shape_predictor)
+
+    scale = 0.5
+    video_capture = cv2.VideoCapture(0)
+    video_capture.set(3, 1920)
+    video_capture.set(4, 1080)
+
+    success, frame = video_capture.read()
+
+    while(success):
+        frame = frame[:,::-1,:].copy()
+        frame_small = cv2.resize(frame, None, fx=scale, fy=scale,interpolation = cv2.INTER_CUBIC)
+        gray_small = cv2.cvtColor(frame_small, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        rects_small = detector(gray_small, 1)
+
+        for (ii, rect_s) in enumerate(rects_small):
+            
+            tmp = np.array([rect_s.left(), rect_s.top(), rect_s.right(), rect_s.bottom()]) / scale
+            tmp = tmp.astype(np.long)
+            rect = dlib.rectangle(tmp[0], tmp[1], tmp[2], tmp[3])
+            shape = predictor(frame, rect)
+            cv2.rectangle(frame, (rect.left(), rect.top()), (rect.right(), rect.bottom()), (255, 0, 0), 2)
+        cv2.imshow("frame", frame)
+        cv2.waitKey(10)
+
+        success, frame = video_capture.read()
+
+        
+
+    
