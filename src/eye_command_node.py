c083cb930f0ecb306625a79b5bbcd8ec766b814a
leon-liangwu <wuliang0301@hotmail.com>
2019-09-06 11:06:07 +0800

    face rect and landmarks

--- src/eye_command_node.py
+++ src/eye_command_node.py
@@ -15,6 +15,8 @@ import cv2
 import numpy as np
 import dlib
 
+import face_utils
+
 # parameters setting
 cap_region_x_begin=0.5  # start point/total width
 cap_region_y_end=0.8  # start point/total width
@@ -30,6 +32,8 @@ learningRate = 0
 isBgCaptured = 0   # bool, whether the background captured
 triggerSwitch = False  # if true, keyborad simulator works
 skinkernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+
+
 
 if __name__ == '__main__':
     
@@ -51,8 +55,6 @@ if __name__ == '__main__':
                             help="Path to camera matrix")
 
     FLAGS, unparsed = parser.parse_known_args()
-
-    print(FLAGS.camera_mat)
 
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(FLAGS.shape_predictor)
@@ -76,9 +78,18 @@ if __name__ == '__main__':
             
             tmp = np.array([rect_s.left(), rect_s.top(), rect_s.right(), rect_s.bottom()]) / scale
             tmp = tmp.astype(np.long)
+
+            # get face rect
             rect = dlib.rectangle(tmp[0], tmp[1], tmp[2], tmp[3])
+            (x, y, w, h) = face_utils.rect_to_bb(rect)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            # get face landmarks
             shape = predictor(frame, rect)
-            cv2.rectangle(frame, (rect.left(), rect.top()), (rect.right(), rect.bottom()), (255, 0, 0), 2)
+            shape = face_utils.shape_to_np(shape)
+            for (x, y) in shape:
+		        cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+            
         cv2.imshow("frame", frame)
         cv2.waitKey(10)
 
